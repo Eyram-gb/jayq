c374f853792abde08ff62dfc9b1f69cfba49437b
eyram <pistisray@gmail.com>
2023-10-21 22:30:05 +0000

    initial mobile setup

--- src/components/ImageModalCarousel.jsx
+++ src/components/ImageModalCarousel.jsx
@@ -7,7 +7,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 const ImageModalCarousel = ({ images, selectedIndex, onClose }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50">
-      <div className="relative z-[999] bg-white">
+      <div className="relative z-[999] md:bg-white">
         <Carousel
           dynamicHeight={false}
           showStatus={false}
@@ -17,12 +17,12 @@ const ImageModalCarousel = ({ images, selectedIndex, onClose }) => {
           swipeable={true}
           emulateTouch={true}
           useKeyboardArrows={true}
-          className="max-w-[80vw] h-[90vh] mx-auto"
+          className="max-w-[100vw] md:max-w-[80vw] max-h-[100vh]  md:h-fit mx-auto"
           renderArrowPrev={(clickHandler, hasPrev, labelPrev) =>
             hasPrev && (
               <button
                 onClick={clickHandler}
-                className="w-7 absolute z-50 top-[50%] left-6 h-7 bg-black bg-opacity-20 rounded-full hover:bg-opacity-50 transition duration-200"
+                className="w-7 absolute z-50 top-[50%] left-6 h-7 hidden md:flex bg-black bg-opacity-20 rounded-full hover:bg-opacity-50 transition duration-200"
               >
                 <img
                   className=" w-5 h-5 mx-auto my-auto pb-1"
@@ -36,7 +36,7 @@ const ImageModalCarousel = ({ images, selectedIndex, onClose }) => {
             hasNext && (
               <button
                 onClick={clickHandler}
-                className="absolute top-[50%] right-6 w-7 h-7 bg-black bg-opacity-20 rounded-full hover:bg-opacity-50 transition duration-200"
+                className="absolute top-[50%] right-6 w-7 h-7 bg-black hidden md:flex bg-opacity-20 rounded-full hover:bg-opacity-50 transition duration-200"
               >
                 <img
                   className="w-5 h-5 mx-auto my-auto pb-1"
@@ -50,7 +50,7 @@ const ImageModalCarousel = ({ images, selectedIndex, onClose }) => {
           {images.map((painting, index) => {
             return (
               <div key={index} className="">
-                <div className="px-4 py-2 flex gap-x-4 items-center">
+                <div className="px-4 py-2 flex gap-x-4 items-center bg-white">
                   <div className="flex gap-x-1 items-center">
                     <p className="text-[14px]">Available For Printing</p>
                     <div className="p-0.5 rounded-full bg-teal-200 w-3 h-3">
@@ -69,10 +69,10 @@ const ImageModalCarousel = ({ images, selectedIndex, onClose }) => {
                   <img
                     src={`/Paintings/${painting}.jpg`}
                     alt={painting}
-                    className="md:max-w-fit md:h-[75vh]"
+                    className="md:max-w-fit md:h-[75vh] h-fit"
                   />
                 </div>
-                <h2>{painting}</h2>
+                <h2 className="bg-white p-2">{painting}</h2>
               </div>
             );
           })}
@@ -80,7 +80,7 @@ const ImageModalCarousel = ({ images, selectedIndex, onClose }) => {
       </div>
       {/* Close Button */}
       <button
-        className="absolute top-6 right-6 text-white hover:text-gray-300 cursor-pointer z-50 bg-white rounded-full h-8 w-8 p-2"
+        className="absolute md:top-6 md:right-6 top-2 right-2 text-white hover:text-gray-300 cursor-pointer z-[9999] bg-white rounded-full h-8 w-8 p-2"
         onClick={onClose}
       >
         <img src="https://s2.svgbox.net/materialui.svg?ic=close" alt="close" />
