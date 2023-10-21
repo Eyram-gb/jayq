b292347ed4fd634bb4ef6db61fd357ca28061767
eyram <pistisray@gmail.com>
2023-10-21 13:22:14 +0000

    complete desktop modal

--- src/components/ImageModalCarousel.jsx
+++ src/components/ImageModalCarousel.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
@@ -8,35 +9,38 @@ const ImageModalCarousel = ({ images, selectedIndex, onClose }) => {
     <div className="fixed inset-0 flex items-center justify-center z-50">
       <div className="relative z-[999] bg-white">
         <Carousel
-          dynamicHeight={true}
+          dynamicHeight={false}
           showStatus={false}
           showIndicators={false}
           showThumbs={false}
           selectedItem={selectedIndex}
           swipeable={true}
+          emulateTouch={true}
           useKeyboardArrows={true}
-          className="max-w-[80vw] max-h-[85vh] mx-auto"
+          className="max-w-[80vw] h-[90vh] mx-auto"
           renderArrowPrev={(clickHandler, hasPrev, labelPrev) =>
             hasPrev && (
               <button
                 onClick={clickHandler}
-                className="w-6 absolute top-[50%] left-8"
+                className="w-7 absolute z-50 top-[50%] left-6 h-7 bg-black bg-opacity-20 rounded-full hover:bg-opacity-50 transition duration-200"
               >
                 <img
-                  className=""
+                  className=" w-5 h-5 mx-auto my-auto pb-1"
+                  alt="prev"
                   src={`https://s2.svgbox.net/hero-outline.svg?ic=chevron-left`}
                 />
               </button>
             )
           }
-          renderArrowNext={(clickHandler, hasPrev, labelPrev) =>
-            hasPrev && (
+          renderArrowNext={(clickHandler, hasNext, labelNext) =>
+            hasNext && (
               <button
                 onClick={clickHandler}
-                className="absolute top-[50%] w-6 right-6"
+                className="absolute top-[50%] right-6 w-7 h-7 bg-black bg-opacity-20 rounded-full hover:bg-opacity-50 transition duration-200"
               >
                 <img
-                  className=""
+                  className="w-5 h-5 mx-auto my-auto pb-1"
+                  alt="next"
                   src={`https://s2.svgbox.net/hero-outline.svg?ic=chevron-right`}
                 />
               </button>
@@ -45,29 +49,42 @@ const ImageModalCarousel = ({ images, selectedIndex, onClose }) => {
         >
           {images.map((painting, index) => {
             return (
-              <div key={index} className="my-auto">
-                <div className="flex gap-x-2">
-                  <h2>{painting}</h2>
-                  <button className="text-gray-500 px-3 py-1 text-xs border border-cyan-300">
+              <div key={index} className="">
+                <div className="px-4 py-2 flex gap-x-4 items-center">
+                  <div className="flex gap-x-1 items-center">
+                    <p className="text-[14px]">Available For Printing</p>
+                    <div className="p-0.5 rounded-full bg-teal-200 w-3 h-3">
+                      <img
+                        src="https://s2.svgbox.net/materialui.svg?ic=check&color=fff"
+                        width="20"
+                        height="20"
+                      />
+                    </div>
+                  </div>
+                  <button className="text-white px-3 py-1 w-32 text-xs bg-teal-300">
                     Purchase Artwork
                   </button>
                 </div>
-                <img
-                  src={`/Paintings/${painting}.jpg`}
-                  className="md:max-w-fit md:max-h-[80vh]"
-                />
+                <div>
+                  <img
+                    src={`/Paintings/${painting}.jpg`}
+                    alt={painting}
+                    className="md:max-w-fit md:h-[75vh]"
+                  />
+                </div>
+                <h2>{painting}</h2>
               </div>
             );
           })}
         </Carousel>
-        {/* Close Button */}
-        <button
-          className="absolute top-4 right-4 text-white hover:text-gray-300 cursor-pointer"
-          onClick={onClose}
-        >
-          <img src="https://s2.svgbox.net/materialui.svg?ic=close" alt="close" />
-        </button>
       </div>
+      {/* Close Button */}
+      <button
+        className="absolute top-6 right-6 text-white hover:text-gray-300 cursor-pointer z-50 bg-white rounded-full h-8 w-8 p-2"
+        onClick={onClose}
+      >
+        <img src="https://s2.svgbox.net/materialui.svg?ic=close" alt="close" />
+      </button>
       <div
         className="absolute inset-0 bg-black opacity-70 z-20"
         onClick={onClose}
